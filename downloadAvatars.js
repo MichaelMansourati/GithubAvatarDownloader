470a117830d5a23da06a9f0e6d6890d42e9b6d39
Michael Mansourati <mmansourati@gmail.com>
2017-03-14 02:13:11 +0000

    successfully created URL variable

--- downloadAvatars.js
+++ downloadAvatars.js
@@ -2,7 +2,13 @@ var request = require('request');
 
 console.log('Welcome to the Github Avatar Downloader!');
 
-function getRepoContributors(repoOwner, repoName, cb);
+var GITHUB_USER = "NiceBoy95";
+var GITHUB_TOKEN = "9150e2b1f8a696b70dd8cea72f16e863238cf5af";
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL)
+}
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
